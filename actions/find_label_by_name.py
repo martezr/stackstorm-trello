5d84eaf4277fea7d72365b8a9a464ff038f8655c
Martez Reed <martez.reed@greenreedtech.com>
2022-02-27 10:14:32 -0600

    Add additional fixes

--- actions/find_label_by_name.py
+++ actions/find_label_by_name.py
@@ -8,7 +8,7 @@ class FindLabelByNameAction(action.BaseAction):
 
         labels = []
         board = self._client().get_board(board_id)
-        for label in board.get_lists('all'):
+        for label in board.get_labels('all'):
             if label.name == name:
                 labels.append(label.id)
 
